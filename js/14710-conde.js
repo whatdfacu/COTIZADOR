7fc5ad48de1f1e8596bdaa407c09d951e0fc114d
Conde Facundo <Facundo.Conde@al-enterprise.com>
2021-05-04 12:57:07 -0300

    corrección de cálculos + debug + animaciones

--- js/14710-conde.js
+++ js/14710-conde.js
@@ -1,3 +1,9 @@
+$(document).ready(function(){
+    //Actualizamos la página
+    $("#boxForm").show(1000);
+/*     location.reload(); */
+});
+
 const materiales = [
     {nombre: "PLA", precio:1700, id:1},
     {nombre: "ABS", precio:1500, id:2},
@@ -33,6 +39,8 @@ miFormulario.submit(function(e) {
 
     let materialDeSoporteForm = ($("#customSwitch1").prop('checked'));
 
+    console.log(materialDeSoporteForm);
+
     //PROCESAMIENTO DE INFORMACION
 
 let precioDolar = dolar.find(e => e.nombre=="precio").valor1;
@@ -42,10 +50,8 @@ let hsDolar = dolar.find(e => e.nombre=="precio").valor2;
 let incrementosCantTrabajo = incrementos.find(e => e.nombre=="ganancia").cantTrabajo;
 
 
-
 let metrosPorKg = incrementos.find(e => e.nombre=="ganancia").metrosPorKg;
 let gastoElectricoHora = incrementos.find(e => e.nombre=="ganancia").gastoElectricoHora;
-
 let percentGanancia = incrementos.find(e => e.nombre=="ganancia").percentGanancia;
 
 //input del usuario más warnings
@@ -56,11 +62,13 @@ if(metrosMaterial <= 0 || ""){
     $("#metrosMaterial").css({"color": "red"});
 };
 
+
 let horasDeImpresion = horasDeImpresionForm;
 if(horasDeImpresion <= 0 || ""){
     alert("Debe ingresar un número mayor a 0 en el campo de cantidad de horas de impresión")
     $("#horasDeImpresion").css({"color": "red"});
 };
+
 
 let materialElegido = materialUtilizadoForm;
 if(materialElegido != "PLA" & materialElegido != "ABS" & materialElegido != "TPU" & materialElegido != "NYLON"){
@@ -68,26 +76,73 @@ if(materialElegido != "PLA" & materialElegido != "ABS" & materialElegido != "TPU
     $("#materialUtilizado").css({"color": "red"});
 };
 
+
+
 let precioMaterial = materiales.find(e => e.nombre ==materialElegido).precio;
 
+
 //INCREMENTADOR DE COSTO SEGUN INPUT
-let siNo = materialDeSoporteForm;
-if(materialDeSoporteForm = true){incrementosCantTrabajo++;};
+
+let sumaIncrement = incrementosCantTrabajo + 0.5;
+
+if(materialDeSoporteForm == true){incrementosCantTrabajo = sumaIncrement};
 
 //CUENTAS PRELIMINARES
 let trabajoHsPesos = (precioDolar * hsDolar)*incrementosCantTrabajo;
+
 let costoCantMaterial = (precioMaterial / metrosPorKg) * metrosMaterial;
+
 let gastoElectric = gastoElectricoHora*horasDeImpresion;
+
+
+
 //RESULTADO FINAL
 let resultadoFinal = Math.floor((trabajoHsPesos + costoCantMaterial + gastoElectric) * percentGanancia);
 
+
+
 //Muestra costo en HTML
-$("#nuevaInfo").append(`<div><h2><li style="display: none" id="title">El costo de su impresion sera de ${resultadoFinal} pesos.</li></h2></div>`);
+$("#nuevaInfo").append(`<div><h2><li style="display: none" id="title">El costo de su impresion sera de ${new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'ARS' }).format(resultadoFinal)}.</li></h2></div>`);
 $("#title").slideDown(1000);
 
 $("#formulario").click(function(){
     //Actualizamos la página
-    location.reload();
+    $("#title").slideUp(1000);
+/*     location.reload(); */
 });
+
+/* $("#formulario").(function(){
+    //Actualizamos la página
+   location.reload();
+}); */
     
+let prueba1 = console.log(`metrosMaterial (${metrosMaterial})`);
+
+let prueba2 = console.log(`horasDeImpresion (${horasDeImpresion})`);
+
+let prueba3 = console.log(`materialElegido (${materialElegido})`);
+
+let prueba4 = console.log(`precioMaterial (${precioMaterial})`)
+
+let prueba5 = console.log(`materialDeSoporteForm (${materialDeSoporteForm})`)
+
+let prueba6 = console.log(`trabajoHsPesos (${trabajoHsPesos}) = (precioDolar(${precioDolar})*hsDolar(${hsDolar}))*incrementosCantTrabajo(${incrementosCantTrabajo})`);
+
+let prueba7 = console.log(`costoCantMaterial (${costoCantMaterial}) = (precioMaterial(${precioMaterial})/hsDolar(${hsDolar}))*metrosPorKg(${metrosPorKg})`);
+
+let prueba8 = console.log(`gastoElectric (${gastoElectric}) = gastoElectricoHora(${gastoElectricoHora})*horasDeImpresion(${horasDeImpresion})`);
+
+let prueba9 = console.log(`resultadoFinal (${resultadoFinal}) = (trabajoHsPesos(${trabajoHsPesos})+costoCantMaterial(${costoCantMaterial})+gastoElectric(${gastoElectric}))*percentGanancia(${percentGanancia})`);
+
+prueba1
+prueba2
+prueba3
+prueba4
+prueba5
+prueba6
+prueba7
+prueba8
+prueba9
+
 });
+
