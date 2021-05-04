d53e33564cffb20121483593d5608b6aac57734f
Conde Facundo <Facundo.Conde@al-enterprise.com>
2021-05-04 19:52:04 -0300

    limieza de js + nuevo sistema de animaciones

--- js/14710-conde.js
+++ js/14710-conde.js
@@ -1,141 +1,88 @@
+//Se verifica que el sitio esté listo
 $(document).ready(function(){
+    //Animacion de inicio
     $("#boxForm").slideDown(1000);
+    //Datos de materiales de impresion
+    const materiales = [
+        {nombre: "PLA", precio:1700, id:1},
+        {nombre: "ABS", precio:1500, id:2},
+        {nombre: "TPU", precio:8000, id:3},
+        {nombre: "NYLON", precio:2000, id:4},
+    ]
+    //Datos de Dolar
+    const dolar = [
+        {nombre:"precio", valor1:150, valor2:6.25,},
+    ]
+    //Datos para incrementos de costos
+    const incrementos = [
+        {nombre: "ganancia", percentGanancia: 1.4, gastoElectricoHora:5, cantTrabajo:0.1, metrosPorKg:335,},
+    ]
+    //Se inicializa funcion para submit de formulario
+    var miFormulario = $("#formulario");
+    miFormulario.submit(function(e) {
+            e.preventDefault();
+            let nodoFormulario = e.target;
 
+            //CAPTURA DE INPUT DE USUARIO "Metros de filamento a utilizar"
+            let primerNodo = nodoFormulario.children[0];
+            let metrosMaterialForm = primerNodo.children[1].value;
+            //CAPTURA DE INPUT DE USUARIO "Horas de impresion"
+            let segundoNodo = nodoFormulario.children[1];
+            let horasDeImpresionForm = segundoNodo.children[1].value;
+            //CAPTURA DE INPUT DE USUARIO "Elija material de impresion"      
+            let materialUtilizadoForm = $("select option:selected").val();
+            //CAPTURA DE INPUT DE USUARIO "Utilizar material de soporte"
+            let materialDeSoporteForm = ($("#customSwitch1").prop('checked'));
 
-const materiales = [
-    {nombre: "PLA", precio:1700, id:1},
-    {nombre: "ABS", precio:1500, id:2},
-    {nombre: "TPU", precio:8000, id:3},
-    {nombre: "NYLON", precio:2000, id:4},
-]
-//DOLAR
-const dolar = [
-    {nombre:"precio", valor1:150, valor2:6.25,},
-]
-//INCREMENTOS
-const incrementos = [
-    {nombre: "ganancia", percentGanancia: 1.4, gastoElectricoHora:5, cantTrabajo:0.1, metrosPorKg:335,},
-]
+            //Procesamiento de informacion
+            let precioDolar = dolar.find(e => e.nombre=="precio").valor1;
+            let hsDolar = dolar.find(e => e.nombre=="precio").valor2;
+            let incrementosCantTrabajo = incrementos.find(e => e.nombre=="ganancia").cantTrabajo;
 
-//CAPTURA DE DATOS DE FORMULARIO
+            let metrosPorKg = incrementos.find(e => e.nombre=="ganancia").metrosPorKg;
+            let gastoElectricoHora = incrementos.find(e => e.nombre=="ganancia").gastoElectricoHora;
+            let percentGanancia = incrementos.find(e => e.nombre=="ganancia").percentGanancia;
 
-$("#nuevaInfo").append("");
+            //Reasignacion de varaiables a los input del usuario + warnings
+            let metrosMaterial = metrosMaterialForm;
+            if(metrosMaterial <= 0 || ""){
+                alert("Debe ingresar un número mayor a 0 en el campo de cantidad de metros de material");
+                $("#metrosMaterial").css({"color": "red"});
+            };      
+            let horasDeImpresion = horasDeImpresionForm;
+            if(horasDeImpresion <= 0 || ""){
+                alert("Debe ingresar un número mayor a 0 en el campo de cantidad de horas de impresión")
+                $("#horasDeImpresion").css({"color": "red"});
+            };
+            let materialElegido = materialUtilizadoForm;
+            if(materialElegido != "PLA" & materialElegido != "ABS" & materialElegido != "TPU" & materialElegido != "NYLON"){
+                alert("Opción no disponible, por favor elija entre PLA, ABS, TPU o NYLON en el campo de material a utilizar")
+                $("#materialUtilizado").css({"color": "red"});
+            }; 
 
-var miFormulario = $("#formulario");
+            //Se selecciona define variable de costo de material elegido segun input de usuario                 
+            let precioMaterial = materiales.find(e => e.nombre ==materialElegido).precio;   
+            //Incremento de costo segun input de usuario
+            let sumaIncrement = incrementosCantTrabajo + 0.5;
+            if(materialDeSoporteForm == true){incrementosCantTrabajo = sumaIncrement};
 
-miFormulario.submit(function(e) {
-    e.preventDefault();
-    let nodoFormulario = e.target;
+            //Cuentas preliminares
+            let trabajoHsPesos = (precioDolar * hsDolar)*incrementosCantTrabajo;
+            let costoCantMaterial = (precioMaterial / metrosPorKg) * metrosMaterial;
+            let gastoElectric = gastoElectricoHora*horasDeImpresion;
 
-    let primerNodo = nodoFormulario.children[0];
-    let metrosMaterialForm = primerNodo.children[1].value;
-
-    let segundoNodo = nodoFormulario.children[1];
-    let horasDeImpresionForm = segundoNodo.children[1].value;
+            //Resultado final
+            let resultadoFinal = Math.floor((trabajoHsPesos + costoCantMaterial + gastoElectric) * percentGanancia);
         
-    let materialUtilizadoForm = $("select option:selected").val();
-
-    let materialDeSoporteForm = ($("#customSwitch1").prop('checked'));
-
-    console.log(materialDeSoporteForm);
-
-    //PROCESAMIENTO DE INFORMACION
-
-let precioDolar = dolar.find(e => e.nombre=="precio").valor1;
-
-let hsDolar = dolar.find(e => e.nombre=="precio").valor2;
-
-let incrementosCantTrabajo = incrementos.find(e => e.nombre=="ganancia").cantTrabajo;
-
-
-let metrosPorKg = incrementos.find(e => e.nombre=="ganancia").metrosPorKg;
-let gastoElectricoHora = incrementos.find(e => e.nombre=="ganancia").gastoElectricoHora;
-let percentGanancia = incrementos.find(e => e.nombre=="ganancia").percentGanancia;
-
-//input del usuario más warnings
-
-let metrosMaterial = metrosMaterialForm;
-if(metrosMaterial <= 0 || ""){
-    alert("Debe ingresar un número mayor a 0 en el campo de cantidad de metros de material");
-    $("#metrosMaterial").css({"color": "red"});
-};
-
-
-let horasDeImpresion = horasDeImpresionForm;
-if(horasDeImpresion <= 0 || ""){
-    alert("Debe ingresar un número mayor a 0 en el campo de cantidad de horas de impresión")
-    $("#horasDeImpresion").css({"color": "red"});
-};
-
-
-let materialElegido = materialUtilizadoForm;
-if(materialElegido != "PLA" & materialElegido != "ABS" & materialElegido != "TPU" & materialElegido != "NYLON"){
-    alert("Opción no disponible, por favor elija entre PLA, ABS, TPU o NYLON en el campo de material a utilizar")
-    $("#materialUtilizado").css({"color": "red"});
-};
-
-
-
-let precioMaterial = materiales.find(e => e.nombre ==materialElegido).precio;
-
-
-//INCREMENTADOR DE COSTO SEGUN INPUT
-
-let sumaIncrement = incrementosCantTrabajo + 0.5;
-
-if(materialDeSoporteForm == true){incrementosCantTrabajo = sumaIncrement};
-
-//CUENTAS PRELIMINARES
-let trabajoHsPesos = (precioDolar * hsDolar)*incrementosCantTrabajo;
-
-let costoCantMaterial = (precioMaterial / metrosPorKg) * metrosMaterial;
-
-let gastoElectric = gastoElectricoHora*horasDeImpresion;
-
-
-
-//RESULTADO FINAL
-let resultadoFinal = Math.floor((trabajoHsPesos + costoCantMaterial + gastoElectric) * percentGanancia);
-
-
-
-//Muestra costo en HTML
-$("#nuevaInfo").append(`<div><h2><li style="display: none" id="title">El costo de su impresion sera de ${new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'ARS' }).format(resultadoFinal)}.</li></h2></div>`);
-$("#title").show(1000);
-
-$("#formulario").click(function(){
-        $("#title").slideUp(1000, function(){
-        //Actualizamos la página
-            location.reload()});
-});
-/*     
-let prueba1 = console.log(`metrosMaterial (${metrosMaterial})`);
-
-let prueba2 = console.log(`horasDeImpresion (${horasDeImpresion})`);
-
-let prueba3 = console.log(`materialElegido (${materialElegido})`);
-
-let prueba4 = console.log(`precioMaterial (${precioMaterial})`)
-
-let prueba5 = console.log(`materialDeSoporteForm (${materialDeSoporteForm})`)
-
-let prueba6 = console.log(`trabajoHsPesos (${trabajoHsPesos}) = (precioDolar(${precioDolar})*hsDolar(${hsDolar}))*incrementosCantTrabajo(${incrementosCantTrabajo})`);
-
-let prueba7 = console.log(`costoCantMaterial (${costoCantMaterial}) = (precioMaterial(${precioMaterial})/hsDolar(${hsDolar}))*metrosPorKg(${metrosPorKg})`);
-
-let prueba8 = console.log(`gastoElectric (${gastoElectric}) = gastoElectricoHora(${gastoElectricoHora})*horasDeImpresion(${horasDeImpresion})`);
-
-let prueba9 = console.log(`resultadoFinal (${resultadoFinal}) = (trabajoHsPesos(${trabajoHsPesos})+costoCantMaterial(${costoCantMaterial})+gastoElectric(${gastoElectric}))*percentGanancia(${percentGanancia})`);
-
-prueba1
-prueba2
-prueba3
-prueba4
-prueba5
-prueba6
-prueba7
-prueba8
-prueba9 */
-
-});
-});
+            //Muestra costo en HTML con animación
+            $("#nuevaInfo").append(`<div><h2><li style="display: none" id="title">El costo de su impresion sera de ${new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'ARS' }).format(resultadoFinal)}.</li></h2></div>`);
+            $("#title").show(1000);
+            
+            //Animación final y recarga de sitio para nueva cotización
+            $("#formulario").click(function(){
+                    $("#boxForm").slideUp(1000, function(){
+                    location.reload()
+                        });
+                });
+        });
+    });
